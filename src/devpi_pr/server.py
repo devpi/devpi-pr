786781e2207de2b716c8e57c56f87635cc3b7e17
Florian Schulze <mail@florian-schulze.net>
2018-12-16 16:20:21 +0100

    Implement first state changes and related errors.

--- src/devpi_pr/server.py
+++ src/devpi_pr/server.py
@@ -14,14 +14,38 @@ class MergeStage(PrivateStage):
 
     @classmethod
     def validate_ixconfig(cls, oldconfig, newconfig):
-        messages = []
+        errors = []
+        new_message_count = len(newconfig["messages"])
         if not oldconfig:
             # creating new stage
             assert newconfig["type"] == "merge"
+            if newconfig["state"] != "new":
+                errors.append("A new merge index must have state 'new'")
+        else:
+            old_message_count = len(oldconfig["messages"])
+            if oldconfig["state"] != newconfig["state"]:
+                if new_message_count != old_message_count + 1:
+                    errors.append("A state change on a merge index requires a message")
+            if old_message_count > new_message_count:
+                errors.append("Messages can't be removed from merge index")
+            if oldconfig["messages"] != newconfig["messages"][:old_message_count]:
+                errors.append("Existing messages can't be modified")
+            if list(oldconfig["bases"]) != list(newconfig["bases"]):
+                errors.append("The bases of a merge index can't be changed")
+            if oldconfig["state"] == "new" and newconfig["state"] != "pending":
+                errors.append("The merge index isn't in state 'pending'")
         if len(newconfig["bases"]) != 1:
-            messages.append("A merge index must have exactly one base")
-        if messages:
-            raise InvalidIndexconfig(messages)
+            errors.append("A merge index must have exactly one base")
+        if errors:
+            raise InvalidIndexconfig(errors)
+
+    def modify_ixconfig(self, ixconfig):
+        if ixconfig["state"] == "pending":
+            target = self.xom.model.getstage(ixconfig["bases"][0])
+            if not target.ixconfig.get("push_requests_allowed", False):
+                raise InvalidIndexconfig([
+                    "The target index '%s' doesn't allow "
+                    "push requests" % target.name])
 
 
 @hookimpl
@@ -31,8 +55,11 @@ def devpiserver_get_stage_class():
 
 @hookimpl
 def devpiserver_indexconfig_defaults(index_type):
-    if index_type != "merge":
+    if index_type == "mirror":
         return {}
+    if index_type == "stage":
+        return {
+            'push_requests_allowed': False}
     return {
         'state': 'new',
         'messages': []}
--- src/devpi_pr/tests/test_server.py
+++ src/devpi_pr/tests/test_server.py
@@ -26,22 +26,77 @@ def xom(request, makexom):
 
 
 @pytest.fixture
-def mergeindex(mapp):
+def targetindex(mapp, testapp):
     mapp.create_and_login_user("targetuser")
-    mapp.create_index("targetindex")
+    result = mapp.create_index("targetindex")
+    r = testapp.get_json(result.index)
+    assert r.json["result"]["push_requests_allowed"] is False
+    r = testapp.patch_json(result.index, dict(
+        r.json["result"],
+        push_requests_allowed=True))
+    assert r.json["result"]["push_requests_allowed"] is True
+    return result
+
+
+@pytest.fixture
+def mergeindex(mapp, targetindex):
     mapp.create_and_login_user("mergeuser")
     api = mapp.create_index(
         "+pr-index",
         indexconfig=dict(
             type="merge",
-            bases=["targetuser/targetindex"]))
+            bases=[targetindex.stagename]))
     return api
 
 
-def test_new_merge_index(mergeindex, testapp, xom):
+def test_new_merge_index(mergeindex, targetindex, testapp):
     r = testapp.get_json(mergeindex.index)
     result = r.json['result']
     assert result['type'] == 'merge'
     assert result['acl_upload'] == ['mergeuser']
-    assert result['bases'] == ['targetuser/targetindex']
+    assert result['bases'] == [targetindex.stagename]
     assert result['messages'] == []
+    assert result['state'] == 'new'
+
+
+def test_submit_merge_index_not_allowed(mapp, mergeindex, targetindex, testapp):
+    # first turn off push_requests_allowed
+    mapp.login(targetindex.stagename.split('/')[0], "123")
+    r = testapp.get_json(targetindex.index)
+    r = testapp.patch_json(targetindex.index, dict(
+        r.json["result"],
+        push_requests_allowed=False))
+    assert r.json["result"]["push_requests_allowed"] is False
+    # now try to submit
+    mapp.login(mergeindex.stagename.split('/')[0], "123")
+    r = testapp.patch_json(mergeindex.index, [
+        'state=pending',
+        'messages+=Please accept'], expect_errors=True)
+    assert r.json["message"] == "The target index '%s' doesn't allow push requests" % targetindex.stagename
+
+
+def test_submit_merge_index(mergeindex, targetindex, testapp):
+    r = testapp.patch_json(mergeindex.index, [
+        'state=pending',
+        'messages+=Please accept'])
+    r = testapp.get_json(mergeindex.index)
+    result = r.json['result']
+    assert result['type'] == 'merge'
+    assert result['acl_upload'] == ['mergeuser']
+    assert result['bases'] == [targetindex.stagename]
+    assert result['messages'] == ['Please accept']
+    assert result['state'] == 'pending'
+
+
+def test_accept_new_not_possible(mergeindex, testapp):
+    r = testapp.patch_json(mergeindex.index, [
+        'state=accepted',
+        'messages+=Accept'], expect_errors=True)
+    assert r.json["message"] == "The merge index isn't in state 'pending'"
+
+
+def test_reject_new_not_possible(mergeindex, testapp):
+    r = testapp.patch_json(mergeindex.index, [
+        'state=rejected',
+        'messages+=Accept'], expect_errors=True)
+    assert r.json["message"] == "The merge index isn't in state 'pending'"
